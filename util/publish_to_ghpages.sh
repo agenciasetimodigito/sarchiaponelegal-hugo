52ca262285ea5059ae5b4625917f637079fe0e0b
Denis C de Azevedo <denis.azevedo@gmail.com>
2018-09-13 21:39:55 -0300

    adiciona tema universal

---
+++ util/publish_to_ghpages.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+UPSTREAM=${1:-origin}
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public $UPSTREAM/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Creating CNAME file for gh-pages"
+echo "www.sarchiapone.legal" >> public/CNAME
+
+echo "Updating gh-pages branch"
+DATE=`date +%Y-%m-%d`
+cd public && git add --all && git commit -m "$DATE Publishing to gh-pages (publish.sh)"
+
+git push origin gh-pages
